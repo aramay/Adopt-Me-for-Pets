0482edc820b0f0bf0a1a3b3ef49624a6b88a1cac
aramay <kasper_341@hotmail.com>
2023-12-17 11:40:28 -0800

    Setup pure React components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-console.log("testing///")
+const Pet  = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h1", {}, props.breed),
+        React.createElement("h1", {}, props.animal)
+    ])
+}
 const App = () => {
     return React.createElement(
         "div",
         {},
-        React.createElement("h1", {}, "Adopt Me")
+        [
+            React.createElement("h1", {}, "Adopt Me"),
+            React.createElement(Pet, {
+                animal: "Dog",
+                name: "Luna",
+                breed: "havenese"
+            }),
+            React.createElement(Pet, {
+                animal: "Bird",
+                name: "Pepper",
+                breed: "Cockatiel"
+            }),
+            React.createElement(Pet, {
+                animal: "Cat",
+                name: "Doink",
+                breed: "Mixed"
+            })
+        ]
     )
 }
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App))
+root.render(React.createElement(App)) 
