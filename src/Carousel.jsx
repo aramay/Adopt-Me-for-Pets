14081b9287d3b83f0aec961221e8d4eafbe742c8
aramay <kasper_341@hotmail.com>
2023-12-19 13:30:12 -0800

    Added Carousel class component.

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,8 +1,42 @@
 import { Component } from "react";
 
 class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  handleIndexClick = (e) => {
+    console.log(this);
+    this.setState({
+      active: +e.target.dataset.index,
+    });
+  };
+
   render() {
-    return <h1>Carousel</h1>;
+    const { active } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+              onClick={this.handleIndexClick}
+              data-index={index}
+            />
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
