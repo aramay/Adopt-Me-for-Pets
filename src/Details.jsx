9788059f12173339c55b6e8de7ef744402aa26a0
aramay <kasper_341@hotmail.com>
2023-12-19 10:50:36 -0800

    Implemented Details page

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,33 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
   const { id } = useParams();
-  return <h2>Details page {id} </h2>;
+  const results = useQuery(["details", id], fetchPet);
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <div className="loader">🌀</div>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+  console.log(results.data);
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.breed} - {pet.city} - {pet.state}
+          <button>Adopt Me</button>
+        </h2>
+        <p>{pet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
