f629638ebecc60186f456b138af1060a4485533d
aramay <kasper_341@hotmail.com>
2025-04-11 12:59:39 -0700

    refactor useBreedList custom hook to use fetchBreedList react query

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,44 +1,9 @@
 import { useState, useEffect } from "react";
-
-const localCache = {};
+import { useQuery } from "@tanstack/react-query";
+import fetchBreedList from "./fetchBreedlist";
 
 export default function useBreedList(animal) {
-  const [breedList, setBreedList] = useState([]);
-  const [status, setStatus] = useState("unloaded");
+  const results = useQuery(["breeds", animal], fetchBreedList);
 
-  useEffect(() => {
-    if (!animal) {
-      setBreedList([]);
-    } else if (localCache[animal]) {
-      setBreedList(localCache[animal]);
-    } else {
-      requestBreedList();
-    }
-  }, [animal]);
-
-  async function requestBreedList() {
-    setBreedList([]);
-    setStatus("loading");
-
-    try {
-      const res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-      );
-
-      if (!res.ok) {
-        console.error(`HTTP error - requestBreeList! Status: ${res.status}`);
-        throw new Error(`HTTP error - requestBreeList! Status: ${res.status}`);
-      }
-      // If res.ok - extract data
-      const json = await res.json();
-      localCache[animal] = json.breeds || [];
-      setBreedList(localCache[animal]);
-      setStatus("loaded");
-    } catch (error) {
-      console.error("Fetch error - requestBreeList!", error.message);
-      // Optionally rethrow or handle error
-      throw error;
-    }
-  }
-  return [breedList, status];
+  return [results?.data?.breeds ?? [], results.status];
 }
