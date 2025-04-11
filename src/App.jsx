63a3031c4e6e78587e1c6b638c15b5edd92ba53a
aramay <kasper_341@hotmail.com>
2025-04-11 12:30:10 -0700

    Details component can now get Pet data.

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,31 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
 
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,10 +1,27 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPets from "./fetchPets";
 
 const Details = () => {
   const { id } = useParams();
+  const results = useQuery(["details", id], fetchPets);
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">⛮</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+
   return (
-    <div>
-      <h2>hi! {id}</h2>
+    <div className="details">
+      <h1>{pet.name}</h1>
+      <h2>{`${pet.animal} = ${pet.breed} - ${pet.city}, ${pet.state}`}</h2>
+      <button>Adopt {pet.name}</button>
+      <p>{pet.description}</p>
     </div>
   );
 };
