69c8196d3e9e71f1ba7952a86b6390a289adfcfa
aramay <kasper_341@hotmail.com>
2025-04-12 11:12:18 -0700

    Added Modal component to Details page

--- src/Details.jsx
+++ src/Details.jsx
@@ -3,8 +3,11 @@ import { useQuery } from "@tanstack/react-query";
 import fetchPets from "./fetchPets";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import { useState } from "react";
+import Modal from "./Modal";
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPets);
 
@@ -22,15 +25,28 @@ const Details = () => {
     <div className="details">
       <Carousel images={pet.images} />
       <h1>{pet.name}</h1>
-      <h2>{`${pet.animal} = ${pet.breed} - ${pet.city}, ${pet.state}`}</h2>
-      <button>Adopt {pet.name}</button>
+      <h2>{`${pet.animal} - ${pet.breed} - ${pet.city}, ${pet.state}`}</h2>
+      <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
       <p>{pet.description}</p>
+      {showModal ? (
+        <Modal>
+          <div>
+            <h1>Adopt me</h1>
+            <div className="buttons">
+              <button>Yes</button>
+              <button onClick={() => setShowModal(false)}>No</button>
+            </div>
+          </div>
+        </Modal>
+      ) : null}
     </div>
   );
 };
 
 export default function DetailsErrorBoundary(props) {
-  <ErrorBoundary>
-    <Details {...props} />
-  </ErrorBoundary>;
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
 }
